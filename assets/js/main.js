0a20cd9b7c4ba1162510528003c3fd081b8ebc1a
Adelakun Tobiloba <adelakuntobiloba1@gmail.com>
2020-07-13 09:45:47 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,18 +11,18 @@ window.addEventListener('load', function(){
     next: '.glider-next'
   },
   responsive: [
+    // {
+    //   // screens greater than >= 775px
+    //   breakpoint: 775,
+    //   settings: {
+    //     // Set to `auto` and provide item width to adjust to viewport
+    //     slidesToShow: 'auto',
+    //     slidesToScroll: 'auto',
+    //     itemWidth: 150,
+    //     duration: 0.25
+    //   }
     {
-      // screens greater than >= 775px
-      breakpoint: 775,
-      settings: {
-        // Set to `auto` and provide item width to adjust to viewport
-        slidesToShow: 'auto',
-        slidesToScroll: 'auto',
-        itemWidth: 150,
-        duration: 0.25
-      }
-    },{
-      // screens greater than >= 1024px
+    // screens greater than >= 1024px
       breakpoint: 1024,
       settings: {
         slidesToShow: 2,
